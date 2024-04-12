be92d38b77fe6ab66b4f1b73105debb778e0990f
Alfredo Sierra <alfredosym06@gmail.com>
2024-04-11 18:21:19 -0600

    feat phaser 3, and supp on all devices

--- js/juego.js
+++ js/juego.js
@@ -1,103 +1,157 @@
-var balas, malos, fondoJuego, nave, timer = 0, delay = 400,aparecer ;
-var puntos, vidas , txtpuntos, txtvidas;
-var Iniciar = {
-  preload: function() {
-    juego.load.image('nave','assets/nave.png');
-    juego.load.image('laser','assets/bala.png');
-    juego.load.image('malo','assets/asteroide.png');
-    juego.load.image('bg','assets/fondo.png');
-  },
-  create: function() {
-    fondoJuego = juego.add.tileSprite(0,0,400,540,'bg');
-    nave = juego.add.sprite(40,juego.height / 2,'nave');
-    //punto de apoyo en el centro de la nave
-    nave.anchor.setTo(0.5);
-    //fisicas asrcade del framework
-    juego.physics.startSystem(Phaser.Physics.ARCADE);
-    juego.physics.arcade.enable(nave,true);
-    nave.allowRotation = false;
+let bala, malos, fondoJuego, nave,velocidad=-100, contador = 0, delay = 400, aparecer;
+let windowWith = window.innerWidth * 0.99, WindowHeight = window.innerHeight * 0.97;
+let puntos, vidas, txtpuntos, txtvidas;
+let target = 0;
+let ROTATION_SPEED = 1 * Math.PI; // radians per second
+class Iniciar extends Phaser.Scene {
+  constructor(...args) {
+    super({ key: 'Iniciar', ...args })
+  }
+  preload() {
+    this.load.image('nave', './assets/nave.png');
+    this.load.image('laser', './assets/bala.png');
+    this.load.image('malo', './assets/asteroide.png');
+    this.load.image('bg', './assets/bg.jpg');
+  }
+  create() {
+    fondoJuego = this.add.image(windowWith / 2, WindowHeight / 2, 'bg');
+    nave = this.add.sprite(40, WindowHeight / 2, 'nave');
+    bala = this.physics.add.sprite(40, WindowHeight / 2, 'laser');
+    bala.visible = false;
+    bala.setCollideWorldBounds(true);
+    bala.body.setAllowGravity(false);
+    this.input.on('pointermove', function (pointer) {
+      target = Phaser.Math.Angle.BetweenPoints(nave, pointer);
+    });
+    this.input.on('pointerdown', this.pointerdown, this);
 
-    //crear balas
-    balas = juego.add.group();
-    balas.enableBody = true;
-    balas.setBodyType = Phaser.Physics.ARCADE;
-    balas.createMultiple(20, "laser");
-    balas.setAll('anchor.x',0.5);
-    balas.setAll('anchor.y',1);
-    balas.setAll('checkWorldBounds',true);
-    balas.setAll('outOfBoundsKill',true);
-
-    malos = juego.add.group();
-    malos.enableBody = true;
-    malos.setBodyType = Phaser.Physics.ARCADE;
-    malos.createMultiple(20, "malo");
-    malos.setAll('anchor.x',0.5);
-    malos.setAll('anchor.y',1);
-    malos.setAll('checkWorldBounds',true);
-    malos.setAll('outOfBoundsKill',true);
+    malos = this.physics.add.group({
+      key: 'malo',
+      repeat: 19,
+      allowGravity: false,
+      collideWorldBounds: true,
+      visible: false
+    });
 
     //generar asteroides
-    aparecer = juego.time.events.loop(1500,this.crearEnemigos,this);
+    aparecer = this.time.addEvent({
+      delay: 1500,
+      loop: true,
+      callback: this.crearEnemigos,
+      callbackScope: this
+    });
 
     //sistema de puntos
     puntos = 0;
-    juego.add.text(20,20,"Score: ",{font: "14px Arial" , fill: "#FFF"});
-    txtpuntos = juego.add.text(70,20,"0",{font: "14px Arial" , fill: "#FFF"});
+    this.add.text(20, 20, "Score: ", { font: "14px Arial", fill: "#FFF" });
+    txtpuntos = this.add.text(70, 20, "0", { font: "14px Arial", fill: "#FFF" });
 
     //sistema de vidas
     vidas = 5;
-    juego.add.text(310,20,"Lifes: ",{font: "14px Arial" , fill: "#FFF"});
-    txtvidas = juego.add.text(350,20,"5",{font: "14px Arial" , fill: "#FFF"});
-  },
-  update: function() {
-    fondoJuego.tilePosition.x -= 3;
-    nave.rotation = juego.physics.arcade.angleToPointer(nave);
+    this.add.text(310, 20, "Lifes: ", { font: "14px Arial", fill: "#FFF" });
+    txtvidas = this.add.text(350, 20, "5", { font: "14px Arial", fill: "#FFF" });
+  }
+  update() {
+    nave.rotation = target;
+    nave.rotation = Phaser.Math.Angle.RotateTo(
+      nave.rotation,
+      target,
+      ROTATION_SPEED * 0.001
+    );
 
-    //disparar laser
-    if(juego.input.activePointer.isDown)
-      this.disparar();
+    //colision asteroide laser
+    this.physics.add.overlap(bala, malos, this.colision, null, this);
 
-      //colision asteroide laser
-      juego.physics.arcade.overlap(balas, malos, this.colision, null, this);
-
-      //colision que quita vidas
-      malos.forEachAlive(function (m) {
-        if (m.position.x > 10 && m.position.x < 12) {
-          vidas --;
-          txtvidas.text = vidas;
-        }
-      });
-
-      //game over?
-      if (vidas == 0) {
-        juego.state.start('Terminado');
+    // //colision que quita vidas
+    malos.children.iterate(function (child) {
+      if ( child.visible && child.x == 25) {
+        vidas--;
+        txtvidas.text = vidas;
+        child.visible = false;
+        child.x = windowWith - 40
+        child.y = WindowHeight / 2
       }
+    });
 
-  },
-  disparar: function(){
-    timer = juego.time.now + delay;
-    var bala = balas.getFirstDead();
-    
-    if (juego.time.now < timer && balas.countDead() > 0) {
-      bala.anchor.setTo(0.5);
-      bala.reset(nave.x, nave.y);
-      bala.rotation = juego.physics.arcade.angleToPointer(bala);
-      juego.physics.arcade.moveToPointer(bala,300);
+    // //game over?
+    if (vidas == 0) {
+      this.scene.start('Terminado');
     }
-  },
-  crearEnemigos: function(){
-     var enem = malos.getFirstDead();
-     var num = Math.floor(Math.random() * 10 + 1);
-     enem.reset(400, num * 55);
-     enem.anchor.setTo(0.5);
-     enem.body.velocity.x = -100;
-     enem.checkWorldBounds = true;
-     enem.outOfBoundsKill = true;
-  },
-  colision: function(bala , malo){
-    bala.kill();
-    malo.kill();
-    puntos++;
-    txtpuntos.text = puntos;
+
+  }
+  pointerdown(pointer) {
+    //Se agrega esto para que en celulares cambie la rotacion
+    // de la nave con los clics
+    target = Phaser.Math.Angle.BetweenPoints(nave, pointer);
+    nave.rotation = Phaser.Math.Angle.RotateTo(
+      nave.rotation,
+      target,
+      ROTATION_SPEED * 0.001
+    );
+    bala.enableBody(true,40,WindowHeight/2,true,true);
+    bala.rotation = target;
+    //Agregar el disparar hace que se mas fluido
+    // y que no se repita como en el update
+    
+    bala.x = 40
+    bala.y = WindowHeight / 2
+    this.physics.moveTo(bala, pointer.x, pointer.y, 300);
+  }
+  crearEnemigos() {
+    let enem = malos.getChildren()[contador];
+    let num = Math.floor(Math.random() * (WindowHeight - 40) + 1);
+    enem.enableBody(true, windowWith - 40,num, true, true);
+    enem.body.velocity.x = velocidad;
+    
+    contador++;
+    if (contador >= malos.getLength()) {
+      contador = 0;
+      velocidad -= 50 
+      
+    }
+  }
+  colision(bala, malo) {
+    console.log("colision");
+    if (malo.visible) {
+      puntos++;
+      txtpuntos.text = puntos;
+    }
+    // bala.visible = false;
+    // bala.x = 40
+    // bala.y = WindowHeight / 2
+    bala.disableBody(true, true);
+    malo.disableBody(true, true);
+    // malo.visible = false;
+    // malo.x = windowWith - 40
+    // malo.y = WindowHeight / 2
   }
 };
+
+class Terminado extends Phaser.Scene {
+  constructor(...args) {
+    super({ key: 'Terminado', ...args })
+  }
+  preload () {
+
+  }
+  create() {
+    this.add.text(400, 230, "GAME OVER", { font: "50px Arial", fill: "#FFF" });
+    // this.state.backgroundColor = "#962813";
+    if (confirm('Continue?'))
+      this.scene.start('Iniciar');
+  }
+}
+const config = {
+  type: Phaser.AUTO,
+  width: windowWith,
+  height: WindowHeight,
+  scene: [Iniciar, Terminado],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 }
+    }
+  }
+};
+
+const game = new Phaser.Game(config);
